344f67758f4bc42ea1816a463ae4b1eb3acf99d8
Steven Nguyen <nguyens5@uci.edu>
2018-02-03 00:44:56 -0800

    Changed file structure to separate front-end and back-end

--- web/src/App.js
+++ web/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import { subscribeToTimer, sendChatMessage, receiveChatMessages } from './api';
-import logo from './logo.svg';
+import { sendChatMessage, receiveChatMessages } from './api';
 import './App.css';
 
 class App extends Component {
